8b1a6e1a72446c0e7c3ccd040a6619991bce38ac
Mansur Mukimbekov <mansur20478@gmail.com>
2026-07-29 15:13:13 -0400

    fix: expose functions

--- crates/accelerators/src/ffi/mod.rs
+++ crates/accelerators/src/ffi/mod.rs
@@ -5,12 +5,16 @@
 //! [`crate::types::ZkvmStatus`]. No other logic lives here.
 
 mod blake2;
+mod bls12_381;
+mod bn254;
 mod ecdsa;
 mod hash;
 mod kzg;
 mod modexp;
 
 pub use blake2::*;
+pub use bls12_381::*;
+pub use bn254::*;
 pub use ecdsa::*;
 pub use hash::*;
 pub use kzg::*;
